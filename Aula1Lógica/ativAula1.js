d9e6641fd50e1a5b7029f5a4e6601b2864a59ae8
Gisele Santana <gisy.ccb@gmail.com>
2025-09-05 11:49:51 -0300

    Add files via upload

---
+++ Aula1Lógica/ativAula1.js
@@ -0,0 +1,41 @@
+// Declaração de Variáveis
+let idade = "18";
+let nome = "Santana";
+let feminino = true;
+
+console.log(idade = "20");
+console.log(nome);
+console.log(feminino); 
+
+//Utilizando o operador 'typeof' para verificar o tipo de cada variável declarada.
+console.log(typeof idade);
+console.log(typeof nome);
+console.log(typeof feminino);
+
+//Operadores Aritméticos
+let result1 = 8 + 9;
+let result2 = 8 - 7;
+let result3 = 8 * 4;
+let result4 = 8 / 4 ;
+let result5 = 8 % 2;
+console.log(result1 + " Soma" ,'\n', result2 + " Subtração" ,'\n', result3 + " Multiplicação",'\n', result4 + " Divisão",'\n', result5 + " Resto" );
+
+//Operadores Relacionais
+let relac1 = 8 > 9;
+let relac2 = 8 < 7;
+let relac3 = 8 >= 4;
+let relac4 = 8 <= 4 ;
+let relac5 = 8 == 2;
+let relac6 = 8 === 8;
+let relac7 = 8 != 2;
+console.log(relac1,"\n",relac2,"\n",relac3,"\n",relac4,"\n",relac5,"\n",relac6);
+console.log("O Operador == verifica se dois valores são iguais.",'\n',
+    "Já o operador === verifica igualdade entre valores e tipagem.");
+
+//Operadores Lógicos
+let number1 = 9;
+let number2 = 10;
+console.log(number1 && number2 === 5);
+console.log(number1 != number2);
+console.log(number1 || number2 == 5*2);
+
---
+++ Aula1Lógica/index.js
@@ -0,0 +1,29 @@
+// Definindo variáveis
+//let idade = 10; // Number
+//const nome = "Santana"; // String
+//let casado = true; // Boolean
+//let exemplo1 = null; // Variável nula
+// let exemplo2 = undefined; // Variável indefinida
+//let array = [];
+//let objeto = {};
+
+// Utilizando Operadores Aritméticos
+//let resultado = n1 + n2;
+//console.log(resultado);
+
+//resultado = n1 - n2;
+//resultado = n1 / n2;
+//resultado = n1 * n2;
+//resultado = n1 ** n2;
+//resultado = n1 % n2; // Sobra da divisão: 12/5 sobra 2
+//incrementação ++ ; // Incrementa um número após: 3++ retorna 4
+//decremento -- ; // Decrementa: 3-- retorna 2
+
+// Operadores de atribuição
+//n3 = n3 + 10; // Pode ser escrito da forma abaixo
+//n3 += 10;
+//n3 -= 10;
+//n3 *= 10;
+//n3 /= 10;
+
+
